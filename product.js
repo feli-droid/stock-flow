8f7b89830dd973b16e49df248ecb1a460967aec1
felispider <leidykatherinerojas393@gmail.com>
2026-06-25 19:54:57 -0500

    terminado

---
+++ product.js
@@ -0,0 +1,27 @@
+const URL_BASE= "https://producto-46b68-default-rtdb.firebaseio.com/"
+document.addEventListener("product-submit", (event) => {
+    const urlFood = `${URL_BASE}/food`;
+    const item = event.detail;
+    const res = httpClient(`${urlFood}.json`, item, "POST");
+    res
+    .then((data) => data.json())
+    .then((data) => {
+    item.id = data.name;
+    const res = httpClient(`${urlFood}/${item.id}.json`, item, "PUT");
+    res
+    .then((data) => data.json())
+    .then((data) => console.log)
+    .catch((err) => console.error);
+    })
+    .catch((err) => console.error);
+    });
+  
+  const httpClient = (url, payload, method) => {
+    return fetch (url, {
+    method,
+    headers: {
+    "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+    });
+  };
