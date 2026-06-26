53e730bb6c58e018bcf7191595bf9e3552c60b99
felispider <leidykatherinerojas393@gmail.com>
2026-06-25 19:54:35 -0500

    terminado

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ document.addEventListener("product-submit", (event) => {
   items.push(item);
   localStorage.setItem("items", JSON.stringify(items));
 });
+
+const URL_console="https://stock-flow-226d2-default-rtdb.firebaseio.com/";
+const user={
+  name:"andres felipe",
+  apellido:"rojas avendaño",
+  username:"felis",
+  pasworld:"ajkfhpfhbfuob"
+}
+
+const https_cliente= fetch(`${URL_console}/user.json`,{
+  method:"POST",
+  headers:{
+    "content-type":"applicacion/json"
+  },
+  body:JSON.stringify(user)
+})
+const res= https_cliente.then(data=>data.json());
+res.then(data => console.log).catch(err=>console.error)
